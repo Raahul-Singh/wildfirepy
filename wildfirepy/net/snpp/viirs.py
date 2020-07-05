feddbe5f967fd3d23e48e7bc0354a6195847e435
Raahul Singh <raahul.s18@iiits.in>
2020-07-05 12:33:12 +0530

    Adds file on disk check

--- wildfirepy/net/snpp/viirs.py
+++ wildfirepy/net/snpp/viirs.py
@@ -1,8 +1,12 @@
-from pathlib import Path
+import glob
+import re
+import warnings
 from datetime import datetime
+from pathlib import Path
 from urllib.error import HTTPError
-from wildfirepy.net.snpp import Viirs1KMParser, URLOpenerWithRedirect
+
 from wildfirepy.coordinates.util import SinusoidalCoordinate
+from wildfirepy.net.snpp import URLOpenerWithRedirect, Viirs1KMParser
 
 __all__ = ['Viirs1KMDownloader']
 
@@ -39,7 +43,7 @@ class Viirs1KM:
         h, v = self.converter(latitude, longitude)
         return self.regex_traverser.get_filename(h, v)
 
-    def get_h5(self, *, obsdate: str, latitude: float, 
+    def get_h5(self, *, obsdate: str, latitude: float,
                longitude: float, fmt: str= "%Y-%m-%d", **kwargs):
         """
         Downloads the `h5` file and stores it on the disk.
@@ -69,7 +73,7 @@ class Viirs1KM:
         url = self.base_url + date + filename
         return self.fetch(url=url, filename=filename, **kwargs)
 
-    def get_xml(self, *, obsdate: str, latitude: float, 
+    def get_xml(self, *, obsdate: str, latitude: float,
                 longitude: float, fmt: str= "%Y-%m-%d", **kwargs):
         """
         Downloads the `xml` file and stores it on the disk.
@@ -99,7 +103,7 @@ class Viirs1KM:
         url = self.base_url + date + filename
         return self.fetch(url=url, filename=filename, **kwargs)
 
-    def get_jpg(self, *, obsdate: str, latitude: float, 
+    def get_jpg(self, *, obsdate: str, latitude: float,
                 longitude: float, fmt: str= "%Y-%m-%d", **kwargs):
         """
         Downloads the `jpg` file and stores it on the disk.
@@ -185,14 +189,37 @@ class Viirs1KMDownloader():
     def __init__(self):
         self.surface_client = VNP09GA()
         self.fire_client = VNP14A1()
+        self.converter = SinusoidalCoordinate()
 
-    def get_data(self, *, obsdate: str, latitude: float, 
+    def get_data(self, *, obsdate: str, latitude: float,
                 longitude: float, fmt: str= "%Y-%m-%d", **kwargs):
-        
-        surface = self.surface_client.get_h5(obsdate=obsdate, latitude=latitude, longitude=longitude, **kwargs)
-        
-        fire_data = self.fire_client.get_h5(obsdate=obsdate, latitude=latitude, longitude=longitude, **kwargs)
+
+        all_files = glob.glob(str(path) + "*h5")
+        obsdatetime = datetime.strptime(obsdate, fmt)
+        date = date = f'{obsdatetime.year}{obsdatetime.timetuple().tm_yday}'
+        h, v = self.converter(latitude, longitude)
+        h = str(h) if h > 9 else "0" + str(h)
+        v = str(v) if v > 9 else "0" + str(v)
+
+        surface_files = re.compile(r'.*' + f'VNP09GA.A{date}.(h{h}v{v}).*')
+        match = list(filter(surface_files.match, all_files))
+
+        if len(match):
+            surface = match[0]
+        else:
+            warnings.warn(UserWarning("Surface data for the given information not found on the disk. \
+                                       Downloading the file!"))
+            surface = self.surface_client.get_h5(obsdate=obsdate, latitude=latitude, longitude=longitude, **kwargs)
+
+        fire_files = re.compile(r'.*' + f'VNP14A1.A{date}.(h{h}v{v}).*')
+        match = list(filter(fire_files.match, all_files))
+
+        if len(match):
+            fire = match[0]
+        else:
+            warnings.warn(UserWarning("Fire data for the given information not found on the disk. \
+                                       Downloading the file!"))
+            fire = self.fire_client.get_h5(obsdate=obsdate, latitude=latitude, longitude=longitude, **kwargs)
 
         return {'surface' : surface,
-                'fire data' : fire_data}
-
+                'fire' : fire}
