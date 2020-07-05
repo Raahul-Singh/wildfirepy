9a4e19d930dfa419f1b9bf213fed701f460b8f68
Raahul Singh <raahul.s18@iiits.in>
2020-07-05 12:33:30 +0530

    Adds gis factory

---
+++ wildfirepy/gis/mapfactory.py
@@ -0,0 +1,22 @@
+from wildfirepy.io import Viirs1KmLoader
+
+__all__ = ['MapFactory']
+
+class Io:
+    def __init__(self):
+        self.Viirs1KmLoader = Viirs1KmLoader()
+
+class MapFactory(Io):
+    def __init__(self, files, **kwargs):
+        super().__init__()
+        self.files = files
+        self.kwargs = kwargs
+        
+    def load_data(self):
+        if isinstance(self.files, dict) and len(self.files) == 2:
+            if set(['surface', 'fire']) == self.files.keys():
+                return self.Viirs1KmLoader.get_data(self.files)
+
+        elif set(['latitude', 'longitude', 'obsdate']).issubset(self.kwargs):
+            data = self.Viirs1KmLoader.Viirs1KMDownloader.get_data(**self.kwargs)
+            return self.Viirs1KmLoader.get_data(data)
